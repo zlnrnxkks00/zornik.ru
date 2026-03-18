04744b909ebd986811ae7481bb52d54284629b09
Gnezdilova Margarita <margarita.gnezdilova@kidzania.ru>
2026-03-18 18:41:48 +0300

    fix responsive errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [path.resolve(__dirname, "src/styles")],
+      },
+    },
+  },
 });
